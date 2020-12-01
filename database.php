7771e41d796f57521589b2fcef7667e63bc391c6
Yang Zou <dcwx1016@gmail.com>
2020-12-01 14:42:31 +0000

    update

--- database.php
+++ database.php
@@ -1,8 +1,6 @@
 <?php
 //This file initialises the database connection. Variable set as superglobal to allow for calling it from within functions
 
-$GLOBALS['connection'] = mysqli_connect('localhost','root','', 'auction_house')
-             or die('Error connecting to MySQL server.' . mysql_error());
-mysqli_set_charset($connection, 'utf8mb4'); 
-
-?>
+$GLOBALS['connection'] = mysqli_connect('auctionhouse13.mysql.database.azure.com', 'group13@auctionhouse13', 'Abc123456', 'newschema')
+or die('Error connecting to MySQL server.' . mysql_error());
+mysqli_set_charset($connection, 'utf8mb4');
