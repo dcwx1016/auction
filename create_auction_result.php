fd713c41f15e5fd510b991be6c84f0b877e808be
Yang Zou <dcwx1016@gmail.com>
2020-12-01 20:49:11 +0000

    a

--- create_auction_result.php
+++ create_auction_result.php
@@ -1,15 +1,9 @@
 <?php include_once "header.php"?>
 
-<script type="text/javascript">
- $(window).on('load',function(){
- $('#auctionResult').modal('show');
- });
-</script>
-
 <div class="container my-5">
 <?php
 include_once "function.php";
-echo 'yes?';
+
 if (isDataValid()) {
     $newItem = getItem();
 
