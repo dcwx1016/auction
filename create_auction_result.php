7b9dbe277a4e03cac8e279a43751ad563052aecb
Yang Zou <dcwx1016@gmail.com>
2020-12-01 20:42:18 +0000

    a

--- create_auction_result.php
+++ create_auction_result.php
@@ -1,4 +1,5 @@
 <?php include_once "header.php"?>
+<?php include_once "function.php"?>
 <script type="text/javascript">
  $(window).on('load',function(){
  $('#auctionResult').modal('show');
@@ -6,7 +7,20 @@
 </script>
 
 <div class="container my-5">
-<?php include_once "function.php"?>
+<?php
+if (isDataValid()) {
+    $newItem = getItem();
+
+    if (checkRepetition($newItem)) {
+        saveToDatabase($newItem);
+        echo "<h2>item added</h2>";
+        printItem($newItem);
+    }
+
+}
+// If all is successful, let user know.
+echo ('<div class="text-center">Auction successfully created! <a href="mylistings.php">View your new listing.</a></div>');
+?>
 </div>
 
 
--- function.php
+++ function.php
@@ -107,17 +107,3 @@ function checkRepetition($item)
     return $item;
 
 }
-
-if (isDataValid()) {
-    $newItem = getItem();
-
-    if (checkRepetition($newItem)) {
-        saveToDatabase($newItem);
-        echo "<h2>item added</h2>";
-        printItem($newItem);
-    }
-
-}
-
-// If all is successful, let user know.
-echo ('<div class="text-center">Auction successfully created! <a href="mylistings.php">View your new listing.</a></div>');
