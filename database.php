f751411f6dc341c079117f43efc60206d9da292a
Yang Zou <dcwx1016@gmail.com>
2020-12-01 14:36:41 +0000

    update

--- database.php
+++ database.php
@@ -1,10 +1,8 @@
 <?php
 //This file initialises the database connection. Variable set as superglobal to allow for calling it from within functions
 
-$GLOBALS['connection'] = mysqli_connect('auctionhouse13.mysql.database.azure.com','group13@auctionhouse13','Abc123456', 'newschema')
+$GLOBALS['connection'] = mysqli_connect('localhost','root','', 'auction_house')
              or die('Error connecting to MySQL server.' . mysql_error());
+mysqli_set_charset($connection, 'utf8mb4'); 
 
-//$con=mysqli_init();
-//mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
-//mysqli_real_connect($con, "auctionhouse13.mysql.database.azure.com", "group13@auctionhouse13", {your_password}, {your_database}, 3306);
 ?>
--- place_bid.php
+++ place_bid.php
@@ -1,4 +1,5 @@
 <?php require_once("header.php")?>
+<?php include("send_mail.php");?>
 
 <script type="text/javascript">
     $(window).on('load',function(){
@@ -25,14 +26,37 @@
     $buyer_id_query = "SELECT buyerId FROM buyer WHERE email='$buyer_username'";
     $buyer_id = intval(mysqli_fetch_row(mysqli_query($connection, $buyer_id_query))[0]);
     
+    $update_query = 
+    "SELECT email,firstName FROM buyer 
+    WHERE buyerId = (SELECT cb.buyerId FROM bid AS b JOIN createbid AS cb ON b.bidNo = cb.bidNo WHERE cb.auctionNo='$auction_number' AND b.bidStatus = 1 )";
+    $result_update = mysqli_query($connection,$update_query) or die('result.' . mysql_error());
+    $fetch_buyer = mysqli_fetch_row($result_update);
+    send_email_update($fetch_buyer[0],$fetch_buyer[1],$bid);
+    
+    $watch_query = 
+    "SELECT b.email,b.firstName FROM buyer AS b JOIN watching AS w ON b.buyerId = w.buyerId WHERE w.auctionNo='$auction_number'";
 
+    $result_watch = mysqli_query($connection,$watch_query) or die('result.' . mysql_error());
 
-    $query = "START TRANSACTION; UPDATE bid
-    SET bid.bidStatus = 0
-    WHERE bid.bidNo IN
-    (SELECT bid.bidNo AS matchingBids FROM createbid JOIN bid ON bid.bidNo = createbid.bidNo WHERE auctionNo = $auction_number);
+    while ($watch_row = mysqli_fetch_assoc($result_watch))
+    {
+        $bid_email = $watch_row['email'];
+        $bid_name = $watch_row['firstName'];
+        send_email_watch($bid_email,$bid_name,$bid);
+    }
+   
+    $query = "
+    START TRANSACTION; 
+    UPDATE bid 
+    SET bid.bidStatus = 0 
+    WHERE bid.bidNo IN 
+    (SELECT * FROM 
+    (SELECT bid.bidNo AS matchingBids 
+    FROM createbid JOIN bid ON bid.bidNo = createbid.bidNo WHERE auctionNo = $auction_number)AS final); 
     INSERT INTO bid (bidAmount, bidTime, bidStatus) VALUES ('$bid', now(),1); SET @last_id_in_bid = LAST_INSERT_ID(); INSERT INTO createbid(bidNo, auctionNo, buyerId) VALUES (@last_id_in_bid, '$auction_number', '$buyer_id'); COMMIT;";
     $message = "Bid placed successfully. Redirecting you back...";
     $create_bid = mysqli_multi_query($connection, $query);
-        runModal("Bidding result",$message,"listing.php?item_id=$auction_number");
+    runModal($message,"listing.php?item_id=$auction_number","Bidding result");
+
+
 ?>
