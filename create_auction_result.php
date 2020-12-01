7bbf373d3e9f1b29649ce2ae5a409ff0aec13403
Yang Zou <dcwx1016@gmail.com>
2020-12-01 21:09:29 +0000

    a

--- create_auction_result.php
+++ create_auction_result.php
@@ -1,8 +1,9 @@
 <?php include_once "header.php"?>
-<?php include_once "function.php"?>
+
 <?php echo ('<div class="text-center">Auction successfully created! <a href="mylistings.php">View your new listing.</a></div>'); ?>
 
-<?php
+<?php include_once "function.php;
+
 echo ('something');
 if (isDataValid()) {
     $newItem = getItem();
@@ -17,6 +18,3 @@ if (isDataValid()) {
 // If all is successful, let user know.
 echo ('<div class="text-center">Auction successfully created! <a href="mylistings.php">View your new listing.</a></div>');
 ?>
-
-
-
