786c5ff8e698389f01525abaf977bc9ece8fbb36
Yang Zou <dcwx1016@gmail.com>
2020-12-01 21:00:19 +0000

    a

--- create_auction_result.php
+++ create_auction_result.php
@@ -2,20 +2,20 @@
 <?php include_once "function.php"?>
 <?php echo ('<div class="text-center">Auction successfully created! <a href="mylistings.php">View your new listing.</a></div>'); ?>
 
-<!-- <?php
-//if (isDataValid()) {
-$newItem = getItem();
+<?php
+if (isDataValid()) {
+    $newItem = getItem();
 
-if (checkRepetition($newItem)) {
-    saveToDatabase($newItem);
-    echo "<h2>item added</h2>";
-    printItem($newItem);
+    if (checkRepetition($newItem)) {
+        saveToDatabase($newItem);
+        echo "<h2>item added</h2>";
+        printItem($newItem);
+    }
+
 }
-
-//}
 // If all is successful, let user know.
 echo ('<div class="text-center">Auction successfully created! <a href="mylistings.php">View your new listing.</a></div>');
-?> -->
+?>
 
 
 
